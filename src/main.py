25d760b2fc6325cbdd944cba500517249dc3793d
Smightym8 <michaelspiegel@live.at>
2022-05-30 16:29:50 +0200

    Added dataset

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import csv
+
+data_file = "../data/penguins.csv"
+
+datasets = []
+
+with open(data_file, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for line in reader:
+        datasets.append(line)
+
+for dataset in datasets:
+    print(dataset)
